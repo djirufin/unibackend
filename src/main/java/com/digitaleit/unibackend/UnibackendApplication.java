f26987683f3ad9914a0d541947ae8a9f1e192f55
djirufin <rufin.dji@gmail.com>
2022-12-11 11:32:20 +0000

    first commit

---
+++ src/main/java/com/digitaleit/unibackend/UnibackendApplication.java
@@ -0,0 +1,13 @@
+package com.digitaleit.unibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnibackendApplication.class, args);
+	}
+
+}
